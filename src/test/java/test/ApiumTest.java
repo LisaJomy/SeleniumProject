fa33f3135468ff6759913a00d65bc8207da78882
LisaJomy <lisa.selenium@gmail.com>
2020-05-20 13:39:24 -0400

    This is my first git project

---
+++ src/test/java/test/ApiumTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class ApiumTest {
+	
+	
+@Test
+public void postJira() {
+	System.out.println("postJira");
+	
+}
+
+
+@Test
+public void deleteTwitter() {
+	System.out.println("deleteTwitter");
+}
+	
+
+}
---
+++ src/test/java/test/RestAPITest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	
+@Test
+public void browserAutomation() {
+	System.out.println("browserAutomation");
+}
+	
+@Test
+public void elementUtil() {
+	System.out.println("elementUtil");
+	
+}
+	
+
+}
